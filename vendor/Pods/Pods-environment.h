d0c05a6830db2ca91df1b129cf246c83b9204ba7
Lori M Olson <lori@wndx.com>
2013-06-03 17:58:28 -0600

    Add pods

---
+++ vendor/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// NVSlideMenuController
+#define COCOAPODS_POD_AVAILABLE_NVSlideMenuController
+#define COCOAPODS_VERSION_MAJOR_NVSlideMenuController 1
+#define COCOAPODS_VERSION_MINOR_NVSlideMenuController 5
+#define COCOAPODS_VERSION_PATCH_NVSlideMenuController 0
+
