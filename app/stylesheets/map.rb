323537a18d6588a4d3ca7bf14c391643af730dc8
Lori M Olson <lori@wndx.com>
2016-08-18 16:58:08 -0600

    Remove unused style

--- app/stylesheets/map.rb
+++ app/stylesheets/map.rb
@@ -12,13 +12,6 @@ Teacup::Stylesheet.new(:map_sheet) do
       :full_height
     ]
 
-  style :map_type,
-    height: 25, width: 200,
-    center_x: '50%',
-    top: '15%',  # includes an 8px margin from the bottom
-    autoresizingMask: (UIViewAutoresizingFlexibleLeftMargin |
-                       UIViewAutoresizingFlexibleRightMargin |
-                       UIViewAutoresizingFlexibleTopMargin)
   style :map_type_button,
     height: 28, width: 28,
         center_x: '91%',
